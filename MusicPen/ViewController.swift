0ec3f5cdf67abdd505a0f8ae59e8727d8243ef28
senitf <senitf1@gmail.com>
2021-06-17 20:07:05 +0900

    210617 p99

--- MusicPen/ViewController.swift
+++ MusicPen/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imgOn = UIImage(named: "half.png")
+        imgOff = UIImage(named: "quarter.png")
+        
+        imgView.image = imgOn
     }
-
-
+    
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        if(isZoom){
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else{
+            newWidth = imgView.frame.width * scale
+            newHeight = imgView.frame.height * scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        isZoom = !isZoom
+    }
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn{
+            imgView.image = imgOn
+        }
+        else{
+            imgView.image = imgOff
+        }
+    }
+    
 }
 
