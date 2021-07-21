a415fa2387e37fb3c70eb19389daf08a08e7a0ec
senitf <senitf1@gmail.com>
2021-07-21 22:02:41 +0900

    200721 daily push

---
+++ MusicPen/MusicPen/LoginView.swift
@@ -0,0 +1,117 @@
+//
+//  ContentView.swift
+//  MusicPen
+//
+//  Created by 김민호 on 2021/07/20.
+//
+
+import SwiftUI
+
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+let storedUsername = "seni"
+let storedPassword = "qwe123"
+
+struct LoginView : View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var authenticationDidFail: Bool = false
+    @State var authenticationDidSucceed: Bool = false
+    var body: some View {
+        ZStack{
+            VStack{
+                WelcomeText()
+                UserImage()
+                UsernameTextField(username: $username)
+                PasswordSecureField(password: $password)
+                if authenticationDidFail {
+                    Text("Information not correct. Try again.")
+                        .offset(y: -10)
+                        .foregroundColor(.red)
+                }
+                Button(action: {
+                    print("Button tapped")
+                    if self.username == storedUsername && self.password == storedPassword {
+                        self.authenticationDidSucceed = true
+                        self.authenticationDidFail = false
+                    }
+                    else {
+                        self.authenticationDidFail = true
+                    }
+                }){
+                    LoginButtonContent()
+                }
+            }
+            .padding()
+            if authenticationDidSucceed {
+                Text("Login succeeded!")
+                    .font(.headline)
+                    .frame(width: 250, height: 80)
+                    .background(Color.green)
+                    .cornerRadius(20.0)
+                    .foregroundColor(.white)
+                    .animation(Animation.default)
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
+
+struct WelcomeText: View {
+    var body: some View {
+        Text("Welcome!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct UserImage: View {
+    var body: some View {
+        Image(systemName:"person.fill")
+            .resizable() //Sets the mode by which SwiftUI resizes an image to fit its space.
+            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/) //Constrains this view’s dimensions to the aspect ratio of the given size.
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
+    }
+}
+
+struct LoginButtonContent: View {
+    var body: some View {
+        Text("LOGIN")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 220, height:60)
+            .background(Color.green)
+            .cornerRadius(15.0)
+    }
+}
+
+struct UsernameTextField: View {
+    @Binding var username: String
+    var body: some View {
+        return TextField("Username", text: $username)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+struct PasswordSecureField: View {
+    @Binding var password: String
+    var body: some View {
+        return SecureField("Password", text: $password)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
